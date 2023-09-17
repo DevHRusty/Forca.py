75e5eba10ea272dcc6bfcd3599cc08c2808a27a0
Heitor Ribeiro <144977663+DevHRusty@users.noreply.github.com>
2023-09-17 00:13:19 -0300

    jogo da forca

---
+++ Jogo da forca_py/forca.py
@@ -0,0 +1,52 @@
+
+palavra = "tecnologia" #palavra que vai ser advinhada
+
+letras_usuario = []
+
+chances = 10 #chances que o usuario tem para acertar a palavra
+
+ganhou = False
+
+while True:
+
+    #logica do programa
+
+    for letra in palavra:
+
+        if letra.lower() in letras_usuario:
+
+            print(letra, end=" ")
+
+        else:
+
+            print("_", end=" ")
+
+    print(f"Você tem {chances} chances")
+
+    tentativa = input("Escolha uma letra para adivinhar: ")
+
+    letras_usuario.append(tentativa.lower())
+
+    if tentativa.lower() not in palavra.lower():
+
+        chances -= 1
+
+    ganhou = True
+
+    for letra in palavra:
+
+        if letra.lower() not in letras_usuario:
+
+            ganhou = False
+
+    if chances == 0 or ganhou:
+
+        break
+
+if ganhou:
+
+    print(f"Parabéns, você ganhou. A palavra era: {palavra}")
+
+else:
+
+    print(f"Você perdeu! A palavra era: {palavra}")
